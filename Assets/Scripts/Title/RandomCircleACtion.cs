d7b167d0029874ad17d9390ae4e12810e61335da
yukineko2048 <yukineko2048@gmail.com>
2022-05-12 04:02:58 +0900

    画面内に3Dオブジェクトがランダムに座標指定できるように処理追加 軌跡を描けるように処理追加

--- Assets/Scripts/Title/RandomCircleACtion.cs
+++ Assets/Scripts/Title/RandomCircleACtion.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-public class RandomCircleACtion : MonoBehaviour
+public class RandomCircleAction : MonoBehaviour
 {
     Vector3 tra_pos {get; set;}
     Vector3 pre_tra_pos {get; set;}
     Vector2 screenRes {get; set;}
-    public GameObject panel{get; set;}
-
     void Start()
     {
+        // 計算式
+        // 画面横幅/画面縦幅:画面比率
+        // *10:シーンのUI画面のグリッド数
+        // /2:画面の半分
+        // -0.25:cubeの大きさの半分
         if (Application.platform == RuntimePlatform.Android) {
-            screenRes = new Vector2(Screen.currentResolution.width, panel.GetComponent<RectTransform>().sizeDelta.y);
+            screenRes = new Vector2((float.Parse(Screen.currentResolution.width.ToString())/float.Parse(Screen.currentResolution.height.ToString())*10)/2 - 0.25f, 4.11f);
             Debug.Log(screenRes);
         }
         else {
-            screenRes = new Vector2(Screen.width, panel.GetComponent<RectTransform>().sizeDelta.y);
+            screenRes = new Vector2((float.Parse(Screen.width.ToString())/float.Parse(Screen.height.ToString())*10)/2-0.25f, 4.11f);
             Debug.Log(screenRes);
         }
         tra_pos = this.gameObject.transform.position;
@@ -33,13 +35,8 @@ public class RandomCircleACtion : MonoBehaviour
     // 次に出現させるポジションを作成する
     Vector3 NextNewPosition()
     {
-        float rand_width = Random.Range (-screenRes.x / 2, screenRes.x / 2);
-        float rand_height = Random.Range (-screenRes.y / 2, screenRes.y / 2);
+        float rand_width = Random.Range (-screenRes.x, screenRes.x);
+        float rand_height = Random.Range (-screenRes.y, screenRes.y);
         return new Vector3(rand_width, rand_height, 0);
-    }
-
-    void Update()
-    {
-        
     }
 }
